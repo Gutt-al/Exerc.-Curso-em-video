27ed62036fde8db988625263b6f64119225be099
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:09:05 -0300

    Add files via upload
    Todos os exercícios foram criados no pycharm e traduzido para inglês pelo translate.

---
+++ ex012 calculando descontos.py
@@ -0,0 +1,5 @@
+# ex012 Faça um algoritmo que leia o preço de um
+# produto e mostre seu novo preço com 5% de desconto.
+
+P = float(input('What is the volue of this product? R$:'))
+print('your product with 5% descount and:R$:{:.2f}'.format(P * 0.95))
