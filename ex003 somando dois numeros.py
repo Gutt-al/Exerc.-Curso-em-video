c5f601669fe457625946d977547a3e5fc0afe7ad
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:06:45 -0300

    Add files via upload
    Todos os exercícios foram criados no pycharm e traduzido para inglês pelo translate.

---
+++ ex003 somando dois numeros.py
@@ -0,0 +1,7 @@
+# Crie um script python que leia dois numeros e tente mostrar
+# a soma entre eles.
+
+n1 = int(input('enter your value:'))
+n2 = float(input('enter your value:'))
+s = n1+n2
+print('the sum between:{} it is {} it is {}'.format(n1, n2, s))
