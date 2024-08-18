4f26426c01fe6fc86fc81d356d7b009ce4582872
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:07:30 -0300

    Add files via upload
    Todos os exercícios foram criados no pycharm e traduzido para inglês pelo translate.

---
+++ ex005 antecessor e sucessor.py
@@ -0,0 +1,5 @@
+# ex005 Faça um programa que leia um número interiro e
+# mostre na tela seu sucessor e seu antecessor.
+
+n = int(input('put a number:'))
+print(' your number and {} The predecessor number is {}, and its successor number and {}'.format(n, (n-1), (n+1)))
