af149b7923610845eb3a0fbbb75b1ffeb25a0321
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:03:55 -0300

    Add files via upload
    Todos os exercícios foram criados no pycharm e traduzido para inglês pelo translate.

---
+++ ex001 deixando tudo pronto.py
@@ -0,0 +1,7 @@
+# Crie um Script python que leia o nome de uma pessoa
+# e mostre uma mensagem de boas vindas de acordo
+# com o valor ditado
+
+name = input('Whats your name?')
+print('Hello', name, 'Welcome')
+
