08284a2e360f816cf207d00bab721e34c4ba1166
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:08:22 -0300

    Add files via upload
    Todos os exercícios foram criados no pycharm e traduzido para inglês pelo translate.

---
+++ ex009 tabuada.py
@@ -0,0 +1,14 @@
+# ex009 Faça um programa que leia um número inteiro
+# qualquer e mostre na tela a sua tabuada.
+
+n = int(input('which multiplication table do you want to know: '))
+print("{}x1={}".format(n, n*1))
+print("{}x2={}".format(n, n*2))
+print("{}x3={}".format(n, n*3))
+print("{}x4={}".format(n, n*4))
+print("{}x5={}".format(n, n*5))
+print("{}x6={}".format(n, n*6))
+print("{}x7={}".format(n, n*7))
+print("{}x8={}".format(n, n*8))
+print("{}x9={}".format(n, n*9))
+print("{}x10={}".format(n, n*10))
