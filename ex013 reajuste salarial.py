572b857cb6f247c5b4045e77fe1db55a81dd9f2e
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:09:22 -0300

    Add files via upload

---
+++ ex013 reajuste salarial.py
@@ -0,0 +1,7 @@
+# ex013 Faça um algoritmo que leia o salário de um funcionárío
+# e mostre seu novo salário com 15% de aumento.
+
+S = float(input('what your wage? R$:'))
+print('your wage is this: R$:{:.2f}'.format(S * 1.15))
+
+# Pode calcular a porcentagem assim: Valor X porcentagem / 100 ex: 1300x20/100
