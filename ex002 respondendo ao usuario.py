c531d22c0e4f9d2a058f184ad9d7af367d6b6b0e
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:06:34 -0300

    Add files via upload
    Todos os exercícios foram criados no pycharm e traduzido para inglês pelo translate.

---
+++ ex002 respondendo ao usuario.py
@@ -0,0 +1,8 @@
+# crie um script python que leia o dia, o mes eo ano
+# de nascimento de uma pessoa e mostre uma mensagem
+# com a data formatada
+
+day = input('what day you were born?')
+month = input('What month you were born?')
+year = input('What year you were born?')
+print('You were born in the', month, day, year, 'rigth?')
