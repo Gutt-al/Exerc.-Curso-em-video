d44ab421b7569192a92f8c327b82ccf634a599ca
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:07:57 -0300

    Add files via upload
    Todos os exercícios foram criados no pycharm e traduzido para inglês pelo translate.

---
+++ ex007 media aritmetica.py
@@ -0,0 +1,7 @@
+# ex007 Desenvolva um programa que leia as duas notas de um
+# aluno, calcule e moestre a sua média.
+
+A = float(input('Proof number one:'))
+A1 = float(input('Proff number two:'))
+m = (A+A1)
+print('The two test add up {},the then avarege be: {:.1f}'.format(m, (m/2)))
